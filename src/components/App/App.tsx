833779b2ecbee8d17458c8672c79733921bcd328
ваше имя <dimarik-tixonov@yandex.ru>
2022-04-18 20:37:57 +0300

    add: добавит и подключит шрифты проекта

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import './app.module.scss';
+import Layout from '../../layout';
+import Main from '../../pages/Main';
+
+function App() {
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,7 @@
+function Profile(): JSX.Element {
+  return (
+    <p>dasda</p>
+  );
+}
+
+export default Profile;

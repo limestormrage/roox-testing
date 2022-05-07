7cc45a7b21fdf554da79b3dafee793d32b14ae75
ваше имя <dimarik-tixonov@yandex.ru>
2022-05-07 16:36:17 +0300

    add: добавит динамические стили для кнопок сортировки

--- src/components/Sort/index.tsx
+++ src/components/Sort/index.tsx
@@ -1,10 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import cn from 'classnames';
 import { changeSortType } from '../../store/action';
 import { sortType } from './const';
 import styles from './sort.module.scss';
+import { getSortType } from '../../store/app/selectors';
 
 function Sort(): JSX.Element {
   const dispatch = useDispatch();
+  const currentSortType = useSelector(getSortType);
 
   const handleChangeSort = (type: string) => {
     dispatch(changeSortType(type));
@@ -13,17 +16,23 @@ function Sort(): JSX.Element {
     <div className={styles.container}>
       <h2 className={styles.title}>Сортировка</h2>
       <div className={styles.sortType}>
-        {Object.entries(sortType).map(([name, label]) => (
-          <button
-            key={name}
-            className={styles.button}
-            type="button"
-            aria-label={label}
-            onClick={() => handleChangeSort(name)}
-          >
-            {label}
-          </button>
-        ))}
+        {Object.entries(sortType).map(([name, label]) => {
+          const buttonClass = cn(styles.button, {
+            [styles.buttonActive]: currentSortType === name,
+          });
+
+          return (
+            <button
+              key={name}
+              className={buttonClass}
+              type="button"
+              aria-label={label}
+              onClick={() => handleChangeSort(name)}
+            >
+              {label}
+            </button>
+          );
+        })}
       </div>
     </div>
   );

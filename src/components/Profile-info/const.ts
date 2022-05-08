dfc658abaa9c9b8052ea5b6a50b2170f53413783
ваше имя <dimarik-tixonov@yandex.ru>
2022-05-09 02:58:16 +0300

    add: реализует отправку формы пользователя

---
+++ src/components/Profile-info/const.ts
@@ -0,0 +1,50 @@
+export const formField = {
+  Name: {
+    type: 'text',
+    name: 'name',
+    title: 'Name',
+    required: true,
+  },
+  UserName: {
+    type: 'text',
+    name: 'userName',
+    title: 'User name',
+    required: true,
+  },
+  Email: {
+    type: 'email',
+    name: 'email',
+    title: 'E-mail',
+    required: true,
+  },
+  Street: {
+    type: 'text',
+    name: 'street',
+    title: 'Street',
+    required: true,
+  },
+  City: {
+    type: 'text',
+    name: 'city',
+    title: 'City',
+    required: true,
+  },
+  ZipCode: {
+    type: 'text',
+    name: 'zipCode',
+    title: 'Zip code',
+    required: true,
+  },
+  Phone: {
+    type: 'phone',
+    name: 'phone',
+    title: 'Phone',
+    required: true,
+  },
+  Website: {
+    type: 'text',
+    name: 'website',
+    title: 'Website',
+    required: true,
+  },
+};
--- src/components/Profile-info/index.tsx
+++ src/components/Profile-info/index.tsx
@@ -1,18 +1,77 @@
-import { useState } from 'react';
+import {
+  ChangeEvent, FormEvent, useEffect, useState,
+} from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getUsers } from '../../store/users/selectors';
+import { getUsers, getUsersLoading } from '../../store/users/selectors';
+import Loader from '../loader/loader';
+import { formField } from './const';
 import styles from './profile-info.module.scss';
+
+const initialState = {
+  name: '',
+  userName: '',
+  email: '',
+  street: '',
+  city: '',
+  zipCode: '',
+  phone: '',
+  website: '',
+  comment: '',
+};
 
 function ProfileInfo(): JSX.Element {
   const { id } = useParams();
   const users = useSelector(getUsers);
+  const usersLoading = useSelector(getUsersLoading);
   const currentUser = users.find((user) => user.id === Number(id));
   const [readOnly, setReadOnly] = useState<boolean>(true);
+  const [formState, setFormState] = useState<{ [key: string]: string }>(initialState);
+
+  useEffect(() => {
+    if (currentUser) {
+      setFormState({
+        name: currentUser.name,
+        userName: currentUser.username,
+        email: currentUser.email,
+        street: currentUser.address.street,
+        city: currentUser.address.city,
+        zipCode: currentUser.address.zipcode,
+        phone: currentUser.phone,
+        website: currentUser.website,
+        comment: '',
+      });
+    }
+  }, [currentUser]);
 
   const handleClickEdit = () => {
     setReadOnly(!readOnly);
   };
+
+  const handleChangeForm = ({ target }: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    const json = JSON.stringify(formState);
+
+    // eslint-disable-next-line no-alert
+    alert('Форма успешно отправлена, данные выведены в консоль');
+
+    // eslint-disable-next-line no-console
+    console.log(json);
+
+    setReadOnly(!readOnly);
+  };
+
+  if (usersLoading) {
+    return (<Loader />);
+  }
 
   return (
     <div className={styles.container}>
@@ -27,43 +86,28 @@ function ProfileInfo(): JSX.Element {
 
         </button>
       </div>
-      <form action="#">
+      <form action="#" onSubmit={handleFormSubmit}>
         <div className={styles.formWrapper}>
-          <div className={styles.formItem}>
-            <label htmlFor="name">Name</label>
-            <input className={styles.input} type="text" id="name" value={currentUser?.name} readOnly={readOnly} required />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="user-name">User name</label>
-            <input className={styles.input} type="text" id="user-name" value={currentUser?.username} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="email">E-mail</label>
-            <input className={styles.input} type="email" id="email" value={currentUser?.email} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="user-street">Street</label>
-            <input className={styles.input} type="text" id="user-street" value={currentUser?.address.street} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="user-city">City</label>
-            <input className={styles.input} type="text" id="user-city" value={currentUser?.address.city} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="zip-code">Zip code</label>
-            <input className={styles.input} type="text" id="zip-code" value={currentUser?.address.zipcode} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="user-phone">Phone</label>
-            <input className={styles.input} type="phone" id="user-phone" value={currentUser?.phone} readOnly={readOnly} />
-          </div>
-          <div className={styles.formItem}>
-            <label className="scad" htmlFor="user-website">Website</label>
-            <input className={styles.input} type="text" id="user-website" value={currentUser?.website} readOnly={readOnly} />
-          </div>
+          {Object.values(formField).map(({
+            type, name, title, required,
+          }) => (
+            <div className={styles.formItem} key={title}>
+              <label htmlFor={name}>{title}</label>
+              <input
+                className={styles.input}
+                type={type}
+                id={name}
+                name={name}
+                value={formState[name]}
+                readOnly={readOnly}
+                required={required}
+                onChange={handleChangeForm}
+              />
+            </div>
+          ))}
           <div className={styles.formItem}>
             <label className="scad" htmlFor="user-comment">Comment</label>
-            <textarea id="user-comment" rows={4} readOnly={readOnly} />
+            <textarea id="user-comment" name="comment" rows={4} readOnly={readOnly} value={formState.comment} onChange={handleChangeForm} />
           </div>
         </div>
         <button
